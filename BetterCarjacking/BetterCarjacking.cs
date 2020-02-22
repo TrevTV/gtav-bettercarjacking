0b491d8ad958f484a960d919a4839d364c6378a4
Trevor Jackson <trevorj342@gmail.com>
2020-02-22 00:44:54 -0500

    All The Code

---
+++ BetterCarjacking/BetterCarjacking.cs
@@ -0,0 +1,72 @@
+﻿using GTA;
+using GTA.Native;
+using GTA.Math;
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+using System.Media;
+
+public class BetterCarjacking : Script
+{
+    private Random rand = new Random();
+    public bool isDebug = false; // edit if you are debugging
+
+    public BetterCarjacking()
+    {
+        Tick += OnTick;
+    }
+
+    private void OnTick(object sender, EventArgs e)
+    {
+        // Get all peds within a radius and run code for each
+        foreach (Ped ped in World.GetNearbyPeds(Game.Player.Character, 50))
+        {
+            // Chance of if car attempts escape or flees
+            // To change chance edit the function IsDebug
+            if (rand.Next(0, 100) <= IsDebug())
+            {
+                // Check if player is targeting ped or vehicle and is not a cop and in a vehicle
+                if ((Game.Player.IsTargeting(ped) || Game.Player.TargetedEntity == ped.CurrentVehicle) && ped.IsSittingInVehicle() && !ped.IsInPoliceVehicle && !(ped.CurrentVehicle.GetPedOnSeat(VehicleSeat.Driver) == Game.Player.Character) && !Game.Player.Character.IsSittingInVehicle())
+                {
+                    ped.BlockPermanentEvents = true;
+                    Game.Player.IsTargeting(ped.CurrentVehicle);
+                    // Decelerate
+                    while ((ped.CurrentVehicle.WheelSpeed * 3.16) > 3)
+                    {
+                        ped.CurrentVehicle.Speed = ped.CurrentVehicle.Speed - (1f * 3.16f);
+                    }
+                    Script.Wait(1000);
+                    Function.Call(Hash.TASK_LEAVE_VEHICLE, ped, ped.CurrentVehicle, 256);
+                    while (ped.IsSittingInVehicle())
+                    {
+                        Script.Wait(100);
+                    }
+                    Function.Call(Hash.TASK_REACT_AND_FLEE_PED, ped, Game.Player.Character);
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if (isDebug)
+        {
+            if (Game.Player.WantedLevel > 0) { Game.Player.WantedLevel = 0; } // If debugging player is never wanted
+        }
+    }
+
+    private int IsDebug()
+    {
+        if (isDebug)
+        {
+            return 100; // If debug 100% chance of flee
+        }
+        else
+        {
+            return 50; // If not debug 50% chance of flee
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# bettercarjacking
-A GTA 5 mod that makes it so that peds may just flee instead of driving away when targeted.
+# Better Carjacking
+
+A mod that makes it so that peds may just flee instead of driving away when targeted.<br> <br>
+
+Please submit any issues in the issues tab.
+
+# Building Instructions
+Make sure you have .NET Framework 4.8 installed.<br>
+Open "BetterCarjacking.sln" in Visual Studio. <br>
+Go to Refrences in the Solution Explorer and set the location for ScriptHookVDotNet3<br>
+Go to the Build tab then "Build Better Carjacking"<br> <br>
+
+NOTE: If you have GTA V installed somewhere other than "steamapps\common\Grand Theft Auto V" you may see an error in the output window, ignore it as it is just attempting to copy it to your game folder.
+
+# Installation and Dependencies:
+You need to have ScriptHookV and ScriptHookVdotNet installed.<br>
+Move the BetterCarjacking.dll into your scripts folder
+
+# Usage
+Point a gun at a driver in a car. They have a 50% chance to drive backwards or forwards like normal or they could flee, giving you the car to take.
+
+# Changelog
+### 2.0.0
+<ul>
+	<b>Fixes</b>
+		<ul>
+			<li>Fixed a possible crash if player points gun in driver's seat.</li>
+                        <li>Peds no longer exit vehicle when player is already in vehicle</li>
+		</ul>
+	<b>Features</b>
+		<ul>
+			<li>Debug mode</li>
+            <li>Cars now decelerate instead of abruptly stopping</li>
+            <li>You can now also point at a ped's car to trigger them</li>
+		</ul>
+</ul>
+
+### 1.0.1
+<ul>
+         <li>Fixed a major performance issue due to bad code.</li>
+</ul>
+
+### 1.0.0
+<ul>
+	<li>Initial release.</li>
+</ul>
