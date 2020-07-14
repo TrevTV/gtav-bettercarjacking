5486c636dd85457e1749a68fcc364c0b8fb9d643
Trevor Jackson <trevorj342@gmail.com>
2020-07-14 19:12:47 -0400

    Move if statement into a method

--- BetterCarjacking/BetterCarjacking.cs
+++ BetterCarjacking/BetterCarjacking.cs
@@ -23,28 +23,48 @@ public class BetterCarjacking : Script
         // Get all peds within a radius and run code for each
         foreach (Ped ped in World.GetNearbyPeds(Game.Player.Character, 25f))
         {
-            // Chance of if car attempts escape or flees
-            if (rand.Next(0, 100) <= 100)
+            if (!CanJackPed(ped))
+                return;
+
+            if (!(rand.Next(0, 100) <= 100))
+                return;
+
+            ped.BlockPermanentEvents = true;
+            Game.Player.IsTargeting(ped.CurrentVehicle);
+            // Decelerate
+            while ((ped.CurrentVehicle.WheelSpeed * 3.16) > 3)
             {
-                // Check if player is targeting ped or vehicle and is not a cop and in a vehicle
-                if ((Game.Player.IsTargeting(ped) || Game.Player.TargetedEntity == ped.CurrentVehicle) && ped.IsSittingInVehicle() && !ped.IsInPoliceVehicle && !(ped.CurrentVehicle.GetPedOnSeat(VehicleSeat.Driver) == Game.Player.Character) && !Game.Player.Character.IsSittingInVehicle())
-                {
-                    ped.BlockPermanentEvents = true;
-                    Game.Player.IsTargeting(ped.CurrentVehicle);
-                    // Decelerate
-                    while ((ped.CurrentVehicle.WheelSpeed * 3.16) > 3)
-                    {
-                        ped.CurrentVehicle.Speed = ped.CurrentVehicle.Speed - (1f * 3.16f);
-                    }
-                    Script.Wait(1000);
-                    Function.Call(Hash.TASK_LEAVE_VEHICLE, ped, ped.CurrentVehicle, 256);
-                    while (ped.IsSittingInVehicle())
-                    {
-                        Script.Wait(100);
-                    }
-                    Function.Call(Hash.TASK_REACT_AND_FLEE_PED, ped, Game.Player.Character);
-                }
+                ped.CurrentVehicle.Speed = ped.CurrentVehicle.Speed - (1f * 3.16f);
             }
+            Script.Wait(1000);
+            Function.Call(Hash.TASK_LEAVE_VEHICLE, ped, ped.CurrentVehicle, 256);
+            while (ped.IsSittingInVehicle())
+            {
+                Script.Wait(100);
+            }
+            Function.Call(Hash.TASK_REACT_AND_FLEE_PED, ped, Game.Player.Character);
         }
+    }
+
+    bool CanJackPed(Ped ped)
+    {
+        var pedInVehicle = ped.IsSittingInVehicle();
+        var targeting = Game.Player.IsTargeting(ped) || Game.Player.IsTargeting(ped.CurrentVehicle);
+        var pedInPolice = ped.IsInPoliceVehicle;
+        var driverIsPlayer = ped.CurrentVehicle.GetPedOnSeat(VehicleSeat.Driver) == Game.Player.Character;
+        var playerInVehicle = Game.Player.Character.IsSittingInVehicle();
+
+        if (!pedInVehicle)
+            return false;
+        if (!targeting)
+            return false;
+        if (pedInPolice)
+            return false;
+        if (driverIsPlayer)
+            return false;
+        if (playerInVehicle)
+            return false;
+
+        return true;
     }
 }
